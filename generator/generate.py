c8b467463efa62e2aaf9d9cf28af73cc7bbdcc49
abhishek-singh30-cloud <abhisheksingh04460@gmail.com>
2026-09-02 16:54:16 +0530

    Simplify vnet module in generate.py
    Refactor vnet module definition to remove unnecessary resource group name and location.

--- generator/generate.py
+++ generator/generate.py
@@ -29,24 +29,16 @@ resource "azurerm_resource_group" "rg" {{
   location = "{location}"
 }}
 
-module "vnet" {{
+module "vnet" {
 
   source = "Azure/avm-res-network-virtualnetwork/azurerm"
 
   name = "{vnet}"
 
-  resource_group_name =
-  azurerm_resource_group.rg.name
-
-  location =
-  azurerm_resource_group.rg.location
-
   address_space = [
     "{address_space}"
   ]
-
-}}
-'''
+}
 
 os.makedirs("generated", exist_ok=True)
 
